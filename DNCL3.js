c9729d969246284618966b4a2585304d0cfc799c
Taisuke Fukuno <fukuno@jig.jp>
2025-01-27 06:00:29 +0900

    add option blacketmode

--- DNCL3.js
+++ DNCL3.js
@@ -2,11 +2,9 @@ import { Runtime } from "./Runtime.js";
 import { Parser } from "./Parser.js";
 
 export class DNCL3 {
-  constructor(s, callbackoutput, callbackinput) {
+  constructor(s, callbackoutput, callbackinput, blacketmode = true) {
     this.callbackoutput = callbackoutput;
     this.callbackinput = callbackinput;
-    //const blacketmode = false; // Wirth
-    const blacketmode = true; // DNCL3
     this.parser = new Parser(blacketmode);
     //this.parseTokens(s); // for debug
     this.ast = this.parser.parse(s);
